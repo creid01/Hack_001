4c731f21befd33f7e7030aba9712fc9d6c48a1e5
IloveRoblox4 <IloveRoblox4@users.noreply.github.com>
2019-01-17 10:28:17 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Christopher Reid, Gregory Quick
+ * Author: Christopher Reid, Gregory Quick, Evan Cornwell
  * Date: 2019/17/01
  *
  * A simple hello world program in C
@@ -13,6 +13,7 @@ int main(int argc, char **argv) {
   printf("Christopher, Greg, \n");
   printf("Christopher's major is Computer Science");
   printf("Greg's major is Accounting\n");
+  printf("Evan's major is not anything"\n);
 
   return 0;
 }
